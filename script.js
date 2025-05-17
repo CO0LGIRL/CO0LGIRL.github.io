1252f9f56fd76b9ba0ce879289cd479c2f1f7dba
Egor <egordefovich@gmail.com>
2025-05-17 21:33:47 +0300

    g

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("main section");
-  
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("visible");
+  const sections = document.querySelectorAll("main section");
+  let lastScrollTop = window.scrollY;
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        if (window.scrollY < lastScrollTop) {
+          entry.target.classList.remove("visible");
+          entry.target.classList.add("collapsed");
         }
-      });
-    }, { threshold: 0.3 });
-  
-    sections.forEach(section => {
-      observer.observe(section);
+      }
     });
+  }, { threshold: 0.2 });
+
+  sections.forEach(section => {
+    observer.observe(section);
   });
+
+  window.addEventListener("scroll", () => {
+    const st = window.scrollY;
+
+    if (st > lastScrollTop) {
+    } else {
+    }
+
+    lastScrollTop = st <= 0 ? 0 : st;
+  });
+});
